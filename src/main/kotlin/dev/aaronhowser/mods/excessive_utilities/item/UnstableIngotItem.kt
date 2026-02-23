6a036f66b2c11971c3c7615df745d6ecf8bc1dae
aaron <aaronhowser1@gmail.com>
2026-02-23 02:40:52 -0600

    feat: implement countdown mechanism for Unstable Ingot item

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/item/UnstableIngotItem.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/item/UnstableIngotItem.kt
@@ -4,10 +4,39 @@ import dev.aaronhowser.mods.excessive_utilities.datagen.language.ModItemLang
 import dev.aaronhowser.mods.excessive_utilities.datagen.language.ModLanguageProvider.Companion.toComponent
 import dev.aaronhowser.mods.excessive_utilities.registry.ModDataComponents
 import net.minecraft.network.chat.Component
+import net.minecraft.world.entity.Entity
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
+import net.minecraft.world.level.Level
 
 class UnstableIngotItem(properties: Properties) : Item(properties) {
+
+	override fun inventoryTick(
+		stack: ItemStack,
+		level: Level,
+		entity: Entity,
+		slotId: Int,
+		isSelected: Boolean
+	) {
+		val isStable = !stack.has(ModDataComponents.COUNTDOWN)
+		if (isStable) return
+
+		val countdown = stack.get(ModDataComponents.COUNTDOWN) ?: return
+		if (countdown > 0) {
+			stack.set(ModDataComponents.COUNTDOWN, countdown - 1)
+			return
+		}
+
+		level.explode(
+			entity,
+			entity.x, entity.y, entity.z,
+			4f,
+			false,
+			Level.ExplosionInteraction.MOB
+		)
+
+		stack.count = 0
+	}
 
 	override fun getName(stack: ItemStack): Component {
 		if (stack.has(ModDataComponents.COUNTDOWN)) return super.getName(stack)
