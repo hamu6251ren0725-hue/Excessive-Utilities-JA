aab7fb700e3f6f56a0d4d042d13064d7843d2522
aaron <aaronhowser1@gmail.com>
2026-02-22 19:56:38 -0600

    feat: refactor Ender Quarry upgrade block entity to use parent block reference and update serialization

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/EnderQuarryUpgradeBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/EnderQuarryUpgradeBlockEntity.kt
@@ -1,7 +1,9 @@
 package dev.aaronhowser.mods.excessive_utilities.block.entity
 
+import dev.aaronhowser.mods.aaron.misc.AaronExtensions.isBlock
 import dev.aaronhowser.mods.excessive_utilities.block.base.EnderQuarryUpgrade
 import dev.aaronhowser.mods.excessive_utilities.registry.ModBlockEntityTypes
+import dev.aaronhowser.mods.excessive_utilities.registry.ModBlocks
 import net.minecraft.core.BlockPos
 import net.minecraft.core.HolderLookup
 import net.minecraft.nbt.CompoundTag
@@ -20,20 +22,36 @@ class EnderQuarryUpgradeBlockEntity(
 			setChanged()
 		}
 
-	var quarryPos: BlockPos? = null
+	var parentBlock: BlockPos? = null
 		set(value) {
 			if (field == value) return
 			field = value
 			setChanged()
 		}
 
+	fun getQuarryPos(): BlockPos? {
+		val level = level ?: return null
+		val parent = parentBlock ?: return null
+
+		if (level.getBlockState(parent).isBlock(ModBlocks.ENDER_QUARRY)) {
+			return parent
+		}
+
+		val parentBe = level.getBlockEntity(parent)
+		if (parentBe is EnderQuarryUpgradeBlockEntity) {
+			return parentBe.getQuarryPos()
+		}
+
+		return null
+	}
+
 	override fun saveAdditional(tag: CompoundTag, registries: HolderLookup.Provider) {
 		super.saveAdditional(tag, registries)
 
 		tag.putString(UPGRADE_TAG, upgrade.id)
-		val qPos = quarryPos
-		if (qPos != null) {
-			tag.putLong(QUARRY_POS_TAG, qPos.asLong())
+		val pPos = parentBlock
+		if (pPos != null) {
+			tag.putLong(PARENT_POS_TAG, pPos.asLong())
 		}
 	}
 
@@ -41,14 +59,14 @@ class EnderQuarryUpgradeBlockEntity(
 		super.loadAdditional(tag, registries)
 
 		upgrade = EnderQuarryUpgrade.valueOf(tag.getString(UPGRADE_TAG))
-		if (tag.contains(QUARRY_POS_TAG)) {
-			quarryPos = BlockPos.of(tag.getLong(QUARRY_POS_TAG))
+		if (tag.contains(PARENT_POS_TAG)) {
+			parentBlock = BlockPos.of(tag.getLong(PARENT_POS_TAG))
 		}
 	}
 
 	companion object {
 		const val UPGRADE_TAG = "Upgrade"
-		const val QUARRY_POS_TAG = "QuarryPos"
+		const val PARENT_POS_TAG = "ParentPos"
 	}
 
 }
