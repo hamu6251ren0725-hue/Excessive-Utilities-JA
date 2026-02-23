b470f7ab81f58a9a5ece23c3e1b6f865c21623b6
aaron <aaronhowser1@gmail.com>
2026-02-22 22:35:39 -0600

    add Trash Can block entity type handling and serialization

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/TrashCanBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/TrashCanBlock.kt
@@ -1,6 +1,8 @@
 package dev.aaronhowser.mods.excessive_utilities.block
 
+import dev.aaronhowser.mods.excessive_utilities.block.entity.TrashCanBlockEntity
 import net.minecraft.core.BlockPos
+import net.minecraft.util.StringRepresentable
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.EntityBlock
@@ -11,10 +13,21 @@ class TrashCanBlock(
 	val type: Type
 ) : Block(Properties.ofFullCopy(Blocks.STONE)), EntityBlock {
 
-	override fun newBlockEntity(pos: BlockPos, state: BlockState): BlockEntity? {
-		TODO("Not yet implemented")
+	override fun newBlockEntity(pos: BlockPos, state: BlockState): BlockEntity {
+		val be = TrashCanBlockEntity(pos, state)
+		be.trashCanType = type
+		return be
 	}
 
-	enum class Type { ITEM, FLUID, ENERGY}
+	enum class Type(
+		val id: String
+	) : StringRepresentable {
+		ITEM("ITEM"),
+		FLUID("FLUID"),
+		ENERGY("ENERGY")
+		;
+
+		override fun getSerializedName(): String = id
+	}
 
 }
--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/TrashCanBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/TrashCanBlockEntity.kt
@@ -1,7 +1,10 @@
 package dev.aaronhowser.mods.excessive_utilities.block.entity
 
+import dev.aaronhowser.mods.excessive_utilities.block.TrashCanBlock
 import dev.aaronhowser.mods.excessive_utilities.registry.ModBlockEntityTypes
 import net.minecraft.core.BlockPos
+import net.minecraft.core.HolderLookup
+import net.minecraft.nbt.CompoundTag
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
 
@@ -9,4 +12,17 @@ class TrashCanBlockEntity(
 	pos: BlockPos,
 	state: BlockState
 ) : BlockEntity(ModBlockEntityTypes.ENDER_QUARRY.get(), pos, state) {
+
+	var trashCanType: TrashCanBlock.Type? = null
+
+	override fun loadAdditional(tag: CompoundTag, registries: HolderLookup.Provider) {
+		super.loadAdditional(tag, registries)
+
+
+	}
+
+	companion object {
+		const val TYPE_TAG = "TrashCanType"
+	}
+
 }
