357d1e43d49d5eb4af64e74a70ae3c5c82deb367
aaron <aaronhowser1@gmail.com>
2026-02-22 18:04:32 -0600

    feat: add cost multipliers for Ender Quarry upgrades

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/base/EnderQuarryUpgrade.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/base/EnderQuarryUpgrade.kt
@@ -1,8 +1,19 @@
 package dev.aaronhowser.mods.excessive_utilities.block.base
 
+import dev.aaronhowser.mods.excessive_utilities.config.ServerConfig
 import java.util.function.DoubleSupplier
 
 enum class EnderQuarryUpgrade(
 	val feMultiplierGetter: DoubleSupplier
 ) {
+	SILK_TOUCH(ServerConfig.CONFIG.eqSilkTouchCostMultiplier),
+	FORTUNE_ONE(ServerConfig.CONFIG.eqFortuneOneCostMultiplier),
+	FORTUNE_TWO(ServerConfig.CONFIG.eqFortuneTwoCostMultiplier),
+	FORTUNE_THREE(ServerConfig.CONFIG.eqFortuneThreeCostMultiplier),
+	SPEED_ONE(ServerConfig.CONFIG.eqSpeedOneCostMultiplier),
+	SPEED_TWO(ServerConfig.CONFIG.eqSpeedTwoCostMultiplier),
+	SPEED_THREE(ServerConfig.CONFIG.eqSpeedThreeCostMultiplier),
+	WORLD_HOLE(ServerConfig.CONFIG.eqWorldHoleCostMultiplier)
+
+	;
 }
