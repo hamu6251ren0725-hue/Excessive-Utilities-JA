7b33f9d924660dbf4aeaa681cacff5791a55d4a7
aaron <aaronhowser1@gmail.com>
2026-03-03 08:37:53 -0600

    feat: implement fire handling and block replacement logic in CursedEarthBlock

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/CursedEarthBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/CursedEarthBlock.kt
@@ -3,7 +3,11 @@ package dev.aaronhowser.mods.excessive_utilities.block
 import dev.aaronhowser.mods.aaron.misc.AaronExtensions.chance
 import dev.aaronhowser.mods.aaron.misc.AaronExtensions.isBlock
 import dev.aaronhowser.mods.excessive_utilities.datagen.tag.ModBlockTagsProvider
+import dev.aaronhowser.mods.excessive_utilities.registry.ModBlocks
 import net.minecraft.core.BlockPos
+import net.minecraft.server.level.ServerLevel
+import net.minecraft.tags.BlockTags
+import net.minecraft.util.RandomSource
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
@@ -47,10 +51,45 @@ class CursedEarthBlock : Block(Properties.ofFullCopy(Blocks.GRASS_BLOCK)) {
 			val newState = defaultBlockState().setValue(DISTANCE, distance - 1)
 			level.setBlockAndUpdate(neighborPos, newState)
 		}
+
+		if (level is ServerLevel) {
+			level.scheduleTick(pos, this, 1)
+		}
+	}
+
+	override fun tick(state: BlockState, level: ServerLevel, pos: BlockPos, random: RandomSource) {
+		level.scheduleTick(pos, this, 1)
+
+		handleFire(level, pos, random)
+
 	}
 
 	companion object {
 		val DISTANCE: IntegerProperty = BlockStateProperties.DISTANCE
+
+		private fun handleFire(level: ServerLevel, pos: BlockPos, random: RandomSource) {
+			val stateAbove = level.getBlockState(pos.above())
+			if (!stateAbove.isBlock(BlockTags.FIRE)) return
+
+			if (random.chance(0.2)) {
+				level.setBlockAndUpdate(pos, Blocks.DIRT.defaultBlockState())
+			}
+
+			val randomNearby = BlockPos.randomInCube(random, 10, pos, 5)
+			for (nearbyPos in randomNearby) {
+				if (!level.isLoaded(nearbyPos)) continue
+
+				val stateThere = level.getBlockState(nearbyPos)
+				if (!stateThere.`is`(ModBlocks.CURSED_EARTH)) continue
+
+				val posAboveThere = nearbyPos.above()
+				val stateAboveThere = level.getBlockState(posAboveThere)
+				if (stateAboveThere.canBeReplaced()) {
+					level.setBlockAndUpdate(posAboveThere, stateAboveThere)
+				}
+			}
+
+		}
 	}
 
 }
