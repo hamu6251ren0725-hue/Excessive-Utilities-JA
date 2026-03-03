683e89cf650f617e226cb2b28f41bb9000d1fd78
aaron <aaronhowser1@gmail.com>
2026-03-03 07:44:48 -0600

    those things dont need to be removed each tick

--- src/main/java/dev/aaronhowser/mods/excessive_utilities/mixin/LevelMixin.java
+++ src/main/java/dev/aaronhowser/mods/excessive_utilities/mixin/LevelMixin.java
@@ -7,9 +7,6 @@ import it.unimi.dsi.fastutil.longs.LongOpenHashSet;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Level.class)
 public abstract class LevelMixin implements ChandelierCarrier, MagnumTorchCarrier, SoundMufflerCarrier {
@@ -37,16 +34,6 @@ public abstract class LevelMixin implements ChandelierCarrier, MagnumTorchCarrie
 	@Override
 	public LongOpenHashSet eu$getSoundMufflerBlockPositions() {
 		return this.eu$soundMufflerBlockPositions;
-	}
-
-	@Inject(
-			method = "tickBlockEntities",
-			at = @At("HEAD")
-	)
-	private void eu$tickBlockEntities(CallbackInfo ci) {
-		this.eu$chandelierBlockPositions.clear();
-		this.eu$magnumTorchBlockPositions.clear();
-		this.eu$soundMufflerBlockPositions.clear();
 	}
 
 }
