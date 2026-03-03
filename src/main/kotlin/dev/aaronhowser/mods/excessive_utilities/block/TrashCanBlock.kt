da473c1de243c7a0a94afa1c4381cf38abd8980b
aaron <aaronhowser1@gmail.com>
2026-03-03 11:28:12 -0600

    add facing direction support to TrashCanBlock

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/TrashCanBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/TrashCanBlock.kt
@@ -4,10 +4,12 @@ import dev.aaronhowser.mods.excessive_utilities.block.entity.trash.EnergyTrashCa
 import dev.aaronhowser.mods.excessive_utilities.block.entity.trash.FluidTrashCanBlockEntity
 import dev.aaronhowser.mods.excessive_utilities.block.entity.trash.TrashCanBlockEntity
 import net.minecraft.core.BlockPos
+import net.minecraft.core.Direction
 import net.minecraft.util.StringRepresentable
 import net.minecraft.world.InteractionResult
 import net.minecraft.world.MenuProvider
 import net.minecraft.world.entity.player.Player
+import net.minecraft.world.item.context.BlockPlaceContext
 import net.minecraft.world.level.BlockGetter
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Block
@@ -15,6 +17,9 @@ import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.EntityBlock
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.level.block.state.StateDefinition
+import net.minecraft.world.level.block.state.properties.BlockStateProperties
+import net.minecraft.world.level.block.state.properties.DirectionProperty
 import net.minecraft.world.phys.BlockHitResult
 import net.minecraft.world.phys.shapes.CollisionContext
 import net.minecraft.world.phys.shapes.Shapes
@@ -23,6 +28,22 @@ import net.minecraft.world.phys.shapes.VoxelShape
 class TrashCanBlock(
 	val type: Type
 ) : Block(Properties.ofFullCopy(Blocks.STONE)), EntityBlock {
+
+	init {
+		registerDefaultState(
+			stateDefinition.any()
+				.setValue(FACING, Direction.NORTH)
+		)
+	}
+
+	override fun createBlockStateDefinition(builder: StateDefinition.Builder<Block, BlockState>) {
+		builder.add(FACING)
+	}
+
+	override fun getStateForPlacement(context: BlockPlaceContext): BlockState? {
+		return defaultBlockState()
+			.setValue(FACING, context.horizontalDirection.opposite)
+	}
 
 	override fun newBlockEntity(pos: BlockPos, state: BlockState): BlockEntity? {
 		if (type == Type.CHEST) {
@@ -64,11 +85,18 @@ class TrashCanBlock(
 	}
 
 	override fun getShape(state: BlockState, level: BlockGetter, pos: BlockPos, context: CollisionContext): VoxelShape {
-		return REGULAR_SHAPE
+		return if (type == Type.CHEST) CHEST_SHAPE else REGULAR_SHAPE
 	}
 
 	companion object {
+		val FACING: DirectionProperty = BlockStateProperties.HORIZONTAL_FACING
+
 		val REGULAR_SHAPE: VoxelShape = Shapes.or(
+			box(2.0, 0.0, 2.0, 14.0, 10.0, 14.0),
+			box(1.0, 10.0, 1.0, 15.0, 14.0, 15.0)
+		)
+
+		val CHEST_SHAPE: VoxelShape = Shapes.or(
 			box(2.0, 0.0, 2.0, 14.0, 10.0, 14.0),
 			box(1.0, 10.0, 1.0, 15.0, 14.0, 15.0)
 		)
