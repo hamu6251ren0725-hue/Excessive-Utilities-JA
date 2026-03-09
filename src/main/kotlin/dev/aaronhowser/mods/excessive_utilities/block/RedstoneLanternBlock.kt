55c4241a5ff1fb4c0e656e8c2f3db4052209f722
aaron <aaronhowser1@gmail.com>
2026-03-09 15:17:49 -0500

    add neighborChanged method to RedstoneLanternBlock for dynamic lighting updates

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/RedstoneLanternBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/RedstoneLanternBlock.kt
@@ -9,6 +9,7 @@ import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.block.state.StateDefinition
 import net.minecraft.world.level.block.state.properties.BlockStateProperties
+import net.minecraft.world.level.block.state.properties.BooleanProperty
 import net.minecraft.world.level.block.state.properties.IntegerProperty
 import net.minecraft.world.phys.BlockHitResult
 
@@ -37,8 +38,18 @@ class RedstoneLanternBlock : Block(Properties.ofFullCopy(Blocks.STONE)) {
 		return InteractionResult.SUCCESS
 	}
 
+	override fun neighborChanged(state: BlockState, level: Level, pos: BlockPos, neighborBlock: Block, neighborPos: BlockPos, movedByPiston: Boolean) {
+		val wasLit = state.getValue(LIT)
+		val shouldBeLit = level.hasNeighborSignal(pos)
+
+		if (wasLit != shouldBeLit) {
+			level.setBlockAndUpdate(pos, state.setValue(LIT, shouldBeLit))
+		}
+	}
+
 	companion object {
 		val POWER: IntegerProperty = BlockStateProperties.POWER
+		val LIT: BooleanProperty = BlockStateProperties.LIT
 	}
 
 }
