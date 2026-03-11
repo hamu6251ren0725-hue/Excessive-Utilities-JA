ea8e6d62210d222fa593cc274115a6db9ef951d5
aaron <aaronhowser1@gmail.com>
2026-03-11 08:48:59 -0500

    feat: implement block breaking functionality in DestructionWandItem

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/item/DestructionWandItem.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/item/DestructionWandItem.kt
@@ -3,10 +3,14 @@ package dev.aaronhowser.mods.excessive_utilities.item
 import dev.aaronhowser.mods.aaron.misc.AaronExtensions.isClientSide
 import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
+import net.minecraft.world.InteractionHand
 import net.minecraft.world.entity.ai.attributes.Attributes
+import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.Item
+import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.Level
 import net.minecraft.world.phys.BlockHitResult
+import net.neoforged.neoforge.event.EventHooks
 import net.neoforged.neoforge.event.level.BlockEvent
 
 class DestructionWandItem(
@@ -54,8 +58,37 @@ class DestructionWandItem(
 			val positions = getPositions(level, blockPos, face, wandItem.maxBlocks)
 			if (positions.isEmpty()) return
 
-			isWandActive = true
+			breakBlocks(usedStack, positions, level, player)
+		}
 
+		// Copied from ServerPlayerGameMode#destroyBlock
+		private fun breakBlocks(usedStack: ItemStack, positions: List<BlockPos>, level: Level, player: Player) {
+			isWandActive = true
+			val stackBeforeBreaking = usedStack.copy()
+
+			for (pos in positions) {
+				val blockEntityThere = level.getBlockEntity(pos)
+				if (blockEntityThere != null) continue
+
+				val stateThere = level.getBlockState(pos)
+				usedStack.mineBlock(level, stateThere, pos, player)
+
+				val canHarvest = stateThere.canHarvestBlock(level, pos, player)
+				val shouldRemove = stateThere.onDestroyedByPlayer(level, pos, player, canHarvest, level.getFluidState(pos))
+				if (shouldRemove) {
+					stateThere.block.destroy(level, pos, stateThere)
+				}
+
+				if (canHarvest && shouldRemove) {
+					stateThere.block.playerDestroy(level, player, pos, stateThere, null, usedStack)
+				}
+
+				if (usedStack.isEmpty && !stackBeforeBreaking.isEmpty) {
+					EventHooks.onPlayerDestroyItem(player, stackBeforeBreaking, InteractionHand.MAIN_HAND)
+				}
+			}
+
+			isWandActive = false
 		}
 
 		private fun getPositions(level: Level, origin: BlockPos, face: Direction, maxCount: Int): List<BlockPos> {
