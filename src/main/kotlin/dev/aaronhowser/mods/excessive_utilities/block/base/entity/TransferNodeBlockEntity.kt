f5055fa4ef8de8de70d4bd0c29e833083f02a3c0
aaron <aaronhowser1@gmail.com>
2026-03-11 16:00:58 -0500

    feat: add save and load functionality for upgrade container in TransferNodeBlockEntity

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/base/entity/TransferNodeBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/base/entity/TransferNodeBlockEntity.kt
@@ -1,8 +1,13 @@
 package dev.aaronhowser.mods.excessive_utilities.block.base.entity
 
 import dev.aaronhowser.mods.aaron.container.ImprovedSimpleContainer
+import dev.aaronhowser.mods.aaron.misc.AaronExtensions.loadItems
+import dev.aaronhowser.mods.aaron.misc.AaronExtensions.saveItems
+import dev.aaronhowser.mods.excessive_utilities.block.TransferNodeBlock
 import dev.aaronhowser.mods.excessive_utilities.block.base.ContainerContainer
 import net.minecraft.core.BlockPos
+import net.minecraft.core.HolderLookup
+import net.minecraft.nbt.CompoundTag
 import net.minecraft.world.Container
 import net.minecraft.world.level.block.entity.BlockEntityType
 import net.minecraft.world.level.block.state.BlockState
@@ -13,14 +18,33 @@ abstract class TransferNodeBlockEntity(
 	blockState: BlockState
 ) : GpDrainBlockEntity(type, pos, blockState), ContainerContainer {
 
+	protected val parentPos: BlockPos = this.blockPos.relative(this.blockState.getValue(TransferNodeBlock.PLACED_ON))
+
 	protected val upgradeContainer = ImprovedSimpleContainer(this, UPGRADE_CONTAINER_SIZE)
 
 	override fun getContainers(): List<Container> {
 		return listOf(upgradeContainer)
 	}
 
+	override fun saveAdditional(tag: CompoundTag, registries: HolderLookup.Provider) {
+		super.saveAdditional(tag, registries)
+
+		val upgradeTag = CompoundTag()
+		upgradeTag.saveItems(upgradeContainer, registries)
+		tag.put(UPGRADES_NBT, upgradeTag)
+	}
+
+	override fun loadAdditional(tag: CompoundTag, registries: HolderLookup.Provider) {
+		super.loadAdditional(tag, registries)
+
+		val upgradeTag = tag.getCompound(UPGRADES_NBT)
+		upgradeTag.loadItems(upgradeContainer, registries)
+	}
+
 	companion object {
 		const val UPGRADE_CONTAINER_SIZE = 5
+
+		const val UPGRADES_NBT = "Upgrades"
 	}
 
 }
