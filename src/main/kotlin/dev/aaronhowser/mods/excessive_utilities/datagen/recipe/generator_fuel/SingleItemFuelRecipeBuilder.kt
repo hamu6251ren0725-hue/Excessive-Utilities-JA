aad59fe6b4d3c220bb0d5c595b20cb3ae223fcf3
aaron <aaronhowser1@gmail.com>
2026-03-15 12:46:26 -0500

    add SingleItemFuelRecipeBuilder for generator fuel recipes

---
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/datagen/recipe/generator_fuel/SingleItemFuelRecipeBuilder.kt
@@ -0,0 +1,46 @@
+package dev.aaronhowser.mods.excessive_utilities.datagen.recipe.generator_fuel
+
+import dev.aaronhowser.mods.excessive_utilities.ExcessiveUtilities
+import dev.aaronhowser.mods.excessive_utilities.recipe.generator_fuel.SingleItemFuelRecipe
+import net.minecraft.advancements.AdvancementRequirements
+import net.minecraft.advancements.AdvancementRewards
+import net.minecraft.advancements.Criterion
+import net.minecraft.advancements.critereon.RecipeUnlockedTrigger
+import net.minecraft.data.recipes.RecipeBuilder
+import net.minecraft.data.recipes.RecipeOutput
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.item.Item
+import net.minecraft.world.item.Items
+import net.minecraft.world.item.crafting.Ingredient
+
+class SingleItemFuelRecipeBuilder(
+	val name: String,
+	val generatorType: SingleItemFuelRecipe.GeneratorType,
+	val ingredient: Ingredient,
+	val fePerTick: Int,
+	val duration: Int
+) : RecipeBuilder {
+
+	override fun unlockedBy(name: String, criterion: Criterion<*>): RecipeBuilder = error("Unsupported")
+	override fun group(groupName: String?): RecipeBuilder = error("Unsupported")
+	override fun getResult(): Item = Items.AIR
+
+	override fun save(recipeOutput: RecipeOutput, id: ResourceLocation) {
+		val idString = StringBuilder()
+			.append("generator_fuel/")
+			.append(generatorType.id)
+			.append("/")
+			.append(name)
+
+		val id = ExcessiveUtilities.modResource(idString.toString())
+
+		val advancement = recipeOutput.advancement()
+			.addCriterion("has_the_recipe", RecipeUnlockedTrigger.unlocked(id))
+			.rewards(AdvancementRewards.Builder.recipe(id))
+			.requirements(AdvancementRequirements.Strategy.OR)
+
+		val recipe = SingleItemFuelRecipe(generatorType, ingredient, fePerTick, duration)
+		recipeOutput.accept(id, recipe, advancement.build(id.withPrefix("recipes/generator_fuel/$generatorType/")))
+	}
+
+}
