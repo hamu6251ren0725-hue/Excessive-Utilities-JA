754be351471b571c57868e2c2bc65e359eba8f0f
aaron <aaronhowser1@gmail.com>
2026-03-17 21:19:40 -0500

    feat: implement interaction for EnderPorcupineBlock to open menu for players

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/EnderPorcupineBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/EnderPorcupineBlock.kt
@@ -2,12 +2,17 @@ package dev.aaronhowser.mods.excessive_utilities.block
 
 import dev.aaronhowser.mods.excessive_utilities.block_entity.EnderPorcupineBlockEntity
 import net.minecraft.core.BlockPos
+import net.minecraft.world.InteractionResult
+import net.minecraft.world.MenuProvider
+import net.minecraft.world.entity.player.Player
 import net.minecraft.world.level.BlockGetter
+import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.EntityBlock
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.phys.BlockHitResult
 import net.minecraft.world.phys.shapes.CollisionContext
 import net.minecraft.world.phys.shapes.VoxelShape
 
@@ -19,6 +24,23 @@ class EnderPorcupineBlock : Block(Properties.ofFullCopy(Blocks.STONE)), EntityBl
 
 	override fun getShape(state: BlockState, level: BlockGetter, pos: BlockPos, context: CollisionContext): VoxelShape {
 		return SHAPE
+	}
+
+	override fun useWithoutItem(
+		state: BlockState,
+		level: Level,
+		pos: BlockPos,
+		player: Player,
+		hitResult: BlockHitResult
+	): InteractionResult {
+		val blockEntity = level.getBlockEntity(pos)
+
+		if (blockEntity is MenuProvider) {
+			player.openMenu(blockEntity)
+			return InteractionResult.sidedSuccess(level.isClientSide)
+		}
+
+		return InteractionResult.PASS
 	}
 
 	companion object {
