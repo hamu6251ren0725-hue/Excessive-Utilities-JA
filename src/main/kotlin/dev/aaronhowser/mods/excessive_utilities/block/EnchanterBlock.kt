bd7af4ad6a32fee527177774776f94dfe8627060
aaron <aaronhowser1@gmail.com>
2026-03-17 23:27:01 -0500

    click

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/EnchanterBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/EnchanterBlock.kt
@@ -3,13 +3,30 @@ package dev.aaronhowser.mods.excessive_utilities.block
 import dev.aaronhowser.mods.excessive_utilities.block.base.GpDrainBlock
 import dev.aaronhowser.mods.excessive_utilities.block_entity.base.GpDrainBlockEntity
 import dev.aaronhowser.mods.excessive_utilities.registry.ModBlockEntityTypes
+import net.minecraft.core.BlockPos
+import net.minecraft.world.InteractionResult
+import net.minecraft.world.MenuProvider
+import net.minecraft.world.entity.player.Player
+import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.entity.BlockEntityType
+import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.phys.BlockHitResult
 
 class EnchanterBlock : GpDrainBlock(Properties.ofFullCopy(Blocks.IRON_BLOCK)) {
 
 	override fun getBlockEntityType(): BlockEntityType<out GpDrainBlockEntity> {
 		return ModBlockEntityTypes.ENCHANTER.get()
+	}
+
+	override fun useWithoutItem(state: BlockState, level: Level, pos: BlockPos, player: Player, hitResult: BlockHitResult): InteractionResult {
+		val be = level.getBlockEntity(pos)
+		if (be is MenuProvider) {
+			player.openMenu(be)
+			return InteractionResult.sidedSuccess(level.isClientSide)
+		}
+
+		return InteractionResult.PASS
 	}
 
 }
