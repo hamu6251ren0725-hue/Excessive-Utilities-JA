c0a56f935a0c25e15f76e7aaf81ff8a5ccabde7f
aaron <aaronhowser1@gmail.com>
2026-03-17 20:55:17 -0500

    buttons

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/menu/ender_porcupine_menu/EnderPorcupineMenu.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/menu/ender_porcupine_menu/EnderPorcupineMenu.kt
@@ -1,5 +1,7 @@
 package dev.aaronhowser.mods.excessive_utilities.menu.ender_porcupine_menu
 
+import dev.aaronhowser.mods.aaron.menu.MenuWithButtons
+import dev.aaronhowser.mods.excessive_utilities.block_entity.EnderPorcupineBlockEntity
 import dev.aaronhowser.mods.excessive_utilities.registry.ModMenuTypes
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.inventory.AbstractContainerMenu
@@ -9,9 +11,95 @@ import net.minecraft.world.item.ItemStack
 class EnderPorcupineMenu(
 	containerId: Int,
 	val containerData: ContainerData
-) : AbstractContainerMenu(ModMenuTypes.RESONATOR.get(), containerId) {
+) : AbstractContainerMenu(ModMenuTypes.RESONATOR.get(), containerId), MenuWithButtons {
+
+	init {
+		addDataSlots(containerData)
+	}
+
+	override fun handleButtonPressed(buttonId: Int) {
+		when (buttonId) {
+			INCREASE_MIN_X_BUTTON_ID -> {
+				val current = containerData.get(EnderPorcupineBlockEntity.MIN_X_DATA_INDEX)
+				containerData.set(EnderPorcupineBlockEntity.MIN_X_DATA_INDEX, current + 1)
+			}
+
+			INCREASE_MIN_Y_BUTTON_ID -> {
+				val current = containerData.get(EnderPorcupineBlockEntity.MIN_Y_DATA_INDEX)
+				containerData.set(EnderPorcupineBlockEntity.MIN_Y_DATA_INDEX, current + 1)
+			}
+
+			INCREASE_MIN_Z_BUTTON_ID -> {
+				val current = containerData.get(EnderPorcupineBlockEntity.MIN_Z_DATA_INDEX)
+				containerData.set(EnderPorcupineBlockEntity.MIN_Z_DATA_INDEX, current + 1)
+			}
+
+			DECREASE_MIN_X_BUTTON_ID -> {
+				val current = containerData.get(EnderPorcupineBlockEntity.MIN_X_DATA_INDEX)
+				containerData.set(EnderPorcupineBlockEntity.MIN_X_DATA_INDEX, current - 1)
+			}
+
+			DECREASE_MIN_Y_BUTTON_ID -> {
+				val current = containerData.get(EnderPorcupineBlockEntity.MIN_Y_DATA_INDEX)
+				containerData.set(EnderPorcupineBlockEntity.MIN_Y_DATA_INDEX, current - 1)
+			}
+
+			DECREASE_MIN_Z_BUTTON_ID -> {
+				val current = containerData.get(EnderPorcupineBlockEntity.MIN_Z_DATA_INDEX)
+				containerData.set(EnderPorcupineBlockEntity.MIN_Z_DATA_INDEX, current - 1)
+			}
+
+			INCREASE_MAX_X_BUTTON_ID -> {
+				val current = containerData.get(EnderPorcupineBlockEntity.MAX_X_DATA_INDEX)
+				containerData.set(EnderPorcupineBlockEntity.MAX_X_DATA_INDEX, current + 1)
+			}
+
+			INCREASE_MAX_Y_BUTTON_ID -> {
+				val current = containerData.get(EnderPorcupineBlockEntity.MAX_Y_DATA_INDEX)
+				containerData.set(EnderPorcupineBlockEntity.MAX_Y_DATA_INDEX, current + 1)
+			}
+
+			INCREASE_MAX_Z_BUTTON_ID -> {
+				val current = containerData.get(EnderPorcupineBlockEntity.MAX_Z_DATA_INDEX)
+				containerData.set(EnderPorcupineBlockEntity.MAX_Z_DATA_INDEX, current + 1)
+			}
+
+			DECREASE_MAX_X_BUTTON_ID -> {
+				val current = containerData.get(EnderPorcupineBlockEntity.MAX_X_DATA_INDEX)
+				containerData.set(EnderPorcupineBlockEntity.MAX_X_DATA_INDEX, current - 1)
+			}
+
+			DECREASE_MAX_Y_BUTTON_ID -> {
+				val current = containerData.get(EnderPorcupineBlockEntity.MAX_Y_DATA_INDEX)
+				containerData.set(EnderPorcupineBlockEntity.MAX_Y_DATA_INDEX, current - 1)
+			}
+
+			DECREASE_MAX_Z_BUTTON_ID -> {
+				val current = containerData.get(EnderPorcupineBlockEntity.MAX_Z_DATA_INDEX)
+				containerData.set(EnderPorcupineBlockEntity.MAX_Z_DATA_INDEX, current - 1)
+			}
+		}
+	}
 
 	override fun quickMoveStack(player: Player, index: Int): ItemStack = ItemStack.EMPTY
 	override fun stillValid(player: Player): Boolean = true
+
+	companion object {
+		const val INCREASE_MIN_X_BUTTON_ID = 0
+		const val INCREASE_MIN_Y_BUTTON_ID = 1
+		const val INCREASE_MIN_Z_BUTTON_ID = 2
+
+		const val DECREASE_MIN_X_BUTTON_ID = 3
+		const val DECREASE_MIN_Y_BUTTON_ID = 4
+		const val DECREASE_MIN_Z_BUTTON_ID = 5
+
+		const val INCREASE_MAX_X_BUTTON_ID = 6
+		const val INCREASE_MAX_Y_BUTTON_ID = 7
+		const val INCREASE_MAX_Z_BUTTON_ID = 8
+
+		const val DECREASE_MAX_X_BUTTON_ID = 9
+		const val DECREASE_MAX_Y_BUTTON_ID = 10
+		const val DECREASE_MAX_Z_BUTTON_ID = 11
+	}
 
 }
