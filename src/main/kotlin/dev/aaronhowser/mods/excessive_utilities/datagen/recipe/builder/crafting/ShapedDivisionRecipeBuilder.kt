0d8b9b301b0a37c108701a725f312c153a8e7afd
aaron <aaronhowser1@gmail.com>
2026-03-19 18:57:14 -0500

    builder

---
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/datagen/recipe/builder/crafting/ShapedDivisionRecipeBuilder.kt
@@ -0,0 +1,79 @@
+package dev.aaronhowser.mods.excessive_utilities.datagen.recipe.builder.crafting
+
+import dev.aaronhowser.mods.aaron.misc.AaronExtensions.getDefaultInstance
+import dev.aaronhowser.mods.excessive_utilities.ExcessiveUtilities
+import dev.aaronhowser.mods.excessive_utilities.recipe.crafting.ShapedDivisionRecipe
+import net.minecraft.advancements.AdvancementRequirements
+import net.minecraft.advancements.AdvancementRewards
+import net.minecraft.advancements.Criterion
+import net.minecraft.advancements.critereon.RecipeUnlockedTrigger
+import net.minecraft.data.recipes.RecipeBuilder
+import net.minecraft.data.recipes.RecipeOutput
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.item.Item
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.crafting.Ingredient
+import net.minecraft.world.item.crafting.ShapedRecipePattern
+import net.minecraft.world.level.ItemLike
+
+class ShapedDivisionRecipeBuilder(
+	val output: ItemStack
+) : RecipeBuilder {
+
+	constructor(output: ItemLike) : this(output.getDefaultInstance())
+
+	private val rows = mutableListOf<String>()
+	private val key = mutableMapOf<Char, Ingredient>()
+	private val criteria = mutableMapOf<String, Criterion<*>>()
+
+	fun define(symbol: Char, ingredient: Ingredient): ShapedDivisionRecipeBuilder {
+		key[symbol] = ingredient
+		return this
+	}
+
+	fun pattern(vararg rows: String): ShapedDivisionRecipeBuilder {
+		this.rows.addAll(rows)
+
+		val width = rows.first().length
+		if (rows.any { it.length != width }) {
+			throw IllegalArgumentException("All rows must have the same length")
+		}
+
+		return this
+	}
+
+	override fun unlockedBy(name: String, criterion: Criterion<*>): RecipeBuilder {
+		criteria[name] = criterion
+		return this
+	}
+
+	override fun group(groupName: String?): RecipeBuilder {
+		error("UnstableIngotRecipe does not support recipe groups")
+	}
+
+	override fun getResult(): Item = output.item
+
+	override fun save(recipeOutput: RecipeOutput, id: ResourceLocation) {
+		val idString = StringBuilder()
+
+		idString
+			.append("division/")
+			.append(id.path)
+
+		val id = ExcessiveUtilities.Companion.modResource(idString.toString())
+
+		val advancement = recipeOutput.advancement()
+			.addCriterion("has_the_recipe", RecipeUnlockedTrigger.unlocked(id))
+			.rewards(AdvancementRewards.Builder.recipe(id))
+			.requirements(AdvancementRequirements.Strategy.OR)
+
+		for (criterion in criteria) {
+			advancement.addCriterion(criterion.key, criterion.value)
+		}
+
+		val pattern = ShapedRecipePattern.of(key, rows)
+		val recipe = ShapedDivisionRecipe(pattern, output)
+		recipeOutput.accept(id, recipe, advancement.build(id.withPrefix("recipes/division/")))
+	}
+
+}
