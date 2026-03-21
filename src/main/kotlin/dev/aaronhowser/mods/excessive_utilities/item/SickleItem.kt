1ab19834a541b5a9ac979db20c932ba7ed1c6da8
aaron <aaronhowser1@gmail.com>
2026-03-21 03:43:59 -0500

    Revert "update SickleItem"
    This reverts commit a225fbff6e2bbb6838cf7537bb0ec71e278a6b14.

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/item/SickleItem.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/item/SickleItem.kt
@@ -4,16 +4,14 @@ import dev.aaronhowser.mods.aaron.misc.AaronExtensions.isServerSide
 import dev.aaronhowser.mods.excessive_utilities.datagen.tag.ModBlockTagsProvider
 import dev.aaronhowser.mods.excessive_utilities.registry.ModDataComponents
 import net.minecraft.core.BlockPos
+import net.minecraft.core.component.DataComponents
 import net.minecraft.world.entity.EquipmentSlot
 import net.minecraft.world.entity.LivingEntity
-import net.minecraft.world.item.DiggerItem
-import net.minecraft.world.item.ItemStack
-import net.minecraft.world.item.Tier
-import net.minecraft.world.item.Tiers
+import net.minecraft.world.item.*
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.state.BlockState
 
-class SickleItem(tier: Tier, properties: Properties) : DiggerItem(tier, ModBlockTagsProvider.SICKLE_MINEABLE, properties) {
+class SickleItem(properties: Properties) : Item(properties) {
 
 	override fun mineBlock(
 		stack: ItemStack,
@@ -52,6 +50,10 @@ class SickleItem(tier: Tier, properties: Properties) : DiggerItem(tier, ModBlock
 		fun getDefaultProperties(tier: Tier): Properties {
 			return Properties()
 				.component(
+					DataComponents.TOOL,
+					tier.createToolProperties(ModBlockTagsProvider.SICKLE_MINEABLE)
+				)
+				.component(
 					ModDataComponents.RADIUS.get(),
 					when (tier) {
 						Tiers.WOOD -> 1
@@ -63,8 +65,19 @@ class SickleItem(tier: Tier, properties: Properties) : DiggerItem(tier, ModBlock
 						else -> 1
 					}
 				)
+				.durability(
+					when (tier) {
+						Tiers.WOOD -> 531
+						Tiers.STONE -> 1179
+						Tiers.GOLD -> 288
+						Tiers.IRON -> 2250
+						Tiers.DIAMOND -> 14049
+						Tiers.NETHERITE -> 30074
+						else -> tier.uses
+					}
+				)
 				.attributes(
-					createAttributes(tier, 0f, -3f)
+					DiggerItem.createAttributes(tier, 0f, -3f)
 				)
 		}
 	}
