bbbf8a0ece0868f0b0e42d6640a361f41fecfcb4
aaron <aaronhowser1@gmail.com>
2026-03-25 10:06:09 -0500

    nullable

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/packet/server_to_client/UpdatePlayerWingPacket.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/packet/server_to_client/UpdatePlayerWingPacket.kt
@@ -5,28 +5,31 @@ import dev.aaronhowser.mods.excessive_utilities.ExcessiveUtilities
 import dev.aaronhowser.mods.excessive_utilities.item.AngelRingItem
 import io.netty.buffer.ByteBuf
 import net.minecraft.core.UUIDUtil
+import net.minecraft.network.codec.ByteBufCodecs
 import net.minecraft.network.codec.StreamCodec
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload
 import net.minecraft.world.entity.player.Player
 import net.neoforged.neoforge.network.handling.IPayloadContext
 import java.util.*
+import kotlin.jvm.optionals.getOrNull
 
 class UpdatePlayerWingPacket(
 	val playerUuid: UUID,
-	val wingType: AngelRingItem.Type?
+	val wingType: Optional<AngelRingItem.Type>
 ) : AaronPacket() {
 
-	constructor(player: Player, wingType: AngelRingItem.Type?) : this(player.uuid, wingType)
+	constructor(player: Player, wingType: AngelRingItem.Type?) : this(player.uuid, Optional.ofNullable(wingType))
 
 	override fun type(): CustomPacketPayload.Type<out CustomPacketPayload> {
 		return TYPE
 	}
 
 	override fun handleOnClient(context: IPayloadContext) {
-		if (wingType == null) {
+		val type = wingType.getOrNull()
+		if (type == null) {
 			AngelRingItem.PLAYER_WINGS.remove(playerUuid)
 		} else {
-			AngelRingItem.PLAYER_WINGS[playerUuid] = wingType
+			AngelRingItem.PLAYER_WINGS[playerUuid] = type
 		}
 	}
 
@@ -37,7 +40,7 @@ class UpdatePlayerWingPacket(
 		val STREAM_CODEC: StreamCodec<ByteBuf, UpdatePlayerWingPacket> =
 			StreamCodec.composite(
 				UUIDUtil.STREAM_CODEC, UpdatePlayerWingPacket::playerUuid,
-				AngelRingItem.Type.STREAM_CODEC, UpdatePlayerWingPacket::wingType,
+				ByteBufCodecs.optional(AngelRingItem.Type.STREAM_CODEC), UpdatePlayerWingPacket::wingType,
 				::UpdatePlayerWingPacket
 			)
 	}
