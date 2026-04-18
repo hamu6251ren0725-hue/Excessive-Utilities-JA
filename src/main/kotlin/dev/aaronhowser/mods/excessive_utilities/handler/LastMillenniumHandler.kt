a0919c3ae55cf3ec70f0b6e640097bc0ddbb759e
aaron <aaronhowser1@gmail.com>
2026-04-18 11:53:28 -0500

    implement chunk saving and loading for Last Millennium dimension

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/handler/LastMillenniumHandler.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/handler/LastMillenniumHandler.kt
@@ -3,9 +3,11 @@ package dev.aaronhowser.mods.excessive_utilities.handler
 import dev.aaronhowser.mods.aaron.misc.AaronUtil
 import net.minecraft.core.HolderLookup
 import net.minecraft.nbt.CompoundTag
+import net.minecraft.nbt.Tag
+import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.level.ChunkPos
 import net.minecraft.world.level.saveddata.SavedData
-import java.util.UUID
+import java.util.*
 
 class LastMillenniumHandler : SavedData() {
 
@@ -25,10 +27,52 @@ class LastMillenniumHandler : SavedData() {
 	}
 
 	override fun save(tag: CompoundTag, registries: HolderLookup.Provider): CompoundTag {
-		TODO("Not yet implemented")
+		val listTag = tag.getList(CHUNK_LIST_NBT, Tag.TAG_COMPOUND.toInt())
+
+		for ((uuid, chunkPos) in playerChunks) {
+			val chunkTag = CompoundTag()
+			chunkTag.putUUID(PLAYER_NBT, uuid)
+			chunkTag.putLong(CHUNK_NBT, chunkPos.toLong())
+			listTag.add(chunkTag)
+		}
+
+		tag.put(CHUNK_LIST_NBT, listTag)
+		return tag
 	}
 
 	companion object {
+		const val SAVED_DATA_NAME = "eu_last_millennium"
 
+		const val CHUNK_LIST_NBT = "Chunks"
+		const val PLAYER_NBT = "Player"
+		const val CHUNK_NBT = "Chunk"
+
+		private fun load(tag: CompoundTag, provider: HolderLookup.Provider): LastMillenniumHandler {
+			val data = LastMillenniumHandler()
+
+			val listTag = tag.getList(CHUNK_LIST_NBT, Tag.TAG_COMPOUND.toInt())
+			for (i in listTag.indices) {
+				val coordinateTag = listTag.getCompound(i)
+
+				val uuid = coordinateTag.getUUID(PLAYER_NBT)
+				val chunkLong = coordinateTag.getLong(CHUNK_NBT)
+				val chunkPos = ChunkPos(chunkLong)
+
+				data.playerChunks[uuid] = chunkPos
+			}
+
+			return data
+		}
+
+		fun get(level: ServerLevel): LastMillenniumHandler {
+			if (level != level.server.overworld()) {
+				return get(level.server.overworld())
+			}
+
+			val storage = level.dataStorage
+			val factory = Factory(::LastMillenniumHandler, ::load)
+
+			return storage.computeIfAbsent(factory, SAVED_DATA_NAME)
+		}
 	}
 }
